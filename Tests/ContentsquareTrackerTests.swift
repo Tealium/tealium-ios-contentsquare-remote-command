c555f5084d06ef9ad3e2fac6f21069e4e7785d8f
Jonathan Wong <jonathan.wong@tealium.com>
2020-03-12 20:53:54 -0700

    Added unit tests.

---
+++ Tests/ContentsquareTrackerTests.swift
@@ -0,0 +1,83 @@
+//
+//  ContentsquareTrackerTests.swift
+//  TealiumContentsquareTests
+//
+//  Created by Jonathan Wong on 3/12/20.
+//  Copyright © 2020 Jonathan Wong. All rights reserved.
+//
+
+import XCTest
+@testable import TealiumContentsquare
+#if COCOAPODS
+#else
+    import TealiumRemoteCommands
+#endif
+
+class ContentsquareTrackerTests: XCTestCase {
+
+    let contentsquareTracker = MockContentsquareTracker()
+    var contentsquareCommand: ContentsquareCommand!
+    var remoteCommand: TealiumRemoteCommand!
+    
+    override func setUp() {
+        contentsquareCommand = ContentsquareCommand(contentsquareTracker: contentsquareTracker)
+        remoteCommand = contentsquareCommand.remoteCommand()
+    }
+
+    override func tearDown() {
+        
+    }
+
+    func testScreenViewCalledWithKey() {
+        contentsquareCommand.parseCommands(["sendscreenview"], payload: ["screen_name": "home"])
+        XCTAssertEqual(1, contentsquareTracker.sendScreenViewCallCount)
+    }
+
+    func testScreenViewNotCalledWitouthKey() {
+        contentsquareCommand.parseCommands(["sendscreenview"], payload: ["not_screen_name": "home"])
+        XCTAssertEqual(0, contentsquareTracker.sendScreenViewCallCount)
+    }
+
+    func testTransactionCalledWithKey() {
+        contentsquareCommand.parseCommands(["sendtransaction"], payload: ["transaction": ["price": 1.99,
+                                                                                          "currency": Int(1)]])
+        XCTAssertEqual(1, contentsquareTracker.sendTransactionCallCount)
+    }
+    
+    func testTransactionNotCalledWithoutKey() {
+        contentsquareCommand.parseCommands(["sendtransaction"], payload: ["not_transaction": ["price": 1.99,
+                                                                                          "currency": Int(1)]])
+        XCTAssertEqual(0, contentsquareTracker.sendTransactionCallCount)
+    }
+    
+    func testTransactionCalledWithIdKey() {
+        contentsquareCommand.parseCommands(["sendtransaction"], payload: ["transaction": ["price": 1.99,
+                                                                                          "currency": Int(1),
+                                                                                          "id": "123"]])
+        XCTAssertEqual(1, contentsquareTracker.sendTransactionCallCount)
+    }
+    
+    func testTransactionNotCalledWithoutPriceKey() {
+        contentsquareCommand.parseCommands(["sendtransaction"], payload: ["not_transaction": ["not_price": 1.99,
+                                                                                              "currency": Int(1)]])
+        XCTAssertEqual(0, contentsquareTracker.sendTransactionCallCount)
+    }
+    
+    func testTransactionNotCalledWithoutCurrencyKey() {
+        contentsquareCommand.parseCommands(["sendtransaction"], payload: ["not_transaction": ["price": 1.99,
+                                                                                              "not_currency": Int(1)]])
+        XCTAssertEqual(0, contentsquareTracker.sendTransactionCallCount)
+    }
+    
+    func testDynamicVarCalledWithKey() {
+        contentsquareCommand.parseCommands(["senddynamicvar"], payload: ["dynamic_var": ["not_price": 1.99,
+                                                                                              "currency": Int(1)]])
+        XCTAssertEqual(0, contentsquareTracker.sendTransactionCallCount)
+    }
+    
+    func testDynamicVarNotCalledWithoutKey() {
+        contentsquareCommand.parseCommands(["senddynamicvar"], payload: ["not_dynamic_var": ["price": 1.99,
+                                                                                              "not_currency": Int(1)]])
+        XCTAssertEqual(0, contentsquareTracker.sendTransactionCallCount)
+    }
+}
---
+++ Tests/MockContentsquareTracker.swift
@@ -0,0 +1,56 @@
+////
+////  TealiumContentsquareTests.swift
+////  TealiumContentsquareTests
+////
+////  Created by Jonathan Wong on 3/4/20.
+////  Copyright © 2020 Jonathan Wong. All rights reserved.
+////
+//
+import XCTest
+@testable import TealiumContentsquare
+
+class MockContentsquareTracker: ContentsquareTrackable {
+
+
+    var sendScreenViewCallCount = 0
+    var sendTransactionCallCount = 0
+    var sendDynamicVarCallCount = 0
+    var stopTrackingCallCount = 0
+    var resumeTrackingCallCount = 0
+    var forgetMeCallCount = 0
+    var optInCallCount = 0
+    var optOutCallCount = 0
+    
+    func sendScreenView(screenName: String) {
+        sendScreenViewCallCount = 1
+    }
+    
+    func sendTransaction(price: Double, currency: Int, transactionId: String?) {
+        sendTransactionCallCount = 1
+    }
+
+    func sendDynamicVar(dynamicVar: [String: Any]) {
+        sendDynamicVarCallCount = 1
+    }
+    
+    func stopTracking() {
+        stopTrackingCallCount = 1
+    }
+    
+    func resumeTracking() {
+        resumeTrackingCallCount = 1
+    }
+    
+    func forgetMe() {
+        forgetMeCallCount = 1
+    }
+    
+    func optIn() {
+        optInCallCount = 1
+    }
+    
+    func optOut() {
+        optOutCallCount = 1
+    }
+    
+}
